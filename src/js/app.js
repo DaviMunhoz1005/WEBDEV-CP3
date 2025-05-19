5e29bb5648aa1b67c13f957311e58522d424f11d
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-05-19 18:30:35 -0300

    feat(app.js): Create function to display all products

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 import { products } from "./products.js";
 
+const divProducts = document.getElementById('productContainerDisplay');
+
+function displayProducts() {
+    let html = '';
+    products.forEach(product => {
+        html += `
+        <div class="product">
+            <h2>${product.nome}</h2>
+            <img src="${product.imagem}" alt="Imagem">
+            <p>R$${product.preco}</p>
+            <h5>${product.categoria}</h5>
+            <h4>${product.disponibilidade ? "Disponível" : "Indisponível"}</h4>
+        </div>`;
+    });
+    divProducts.innerHTML = html;
+}
+
+displayProducts();
+
 console.log(products);
