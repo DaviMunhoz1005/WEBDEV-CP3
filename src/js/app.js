158bd553bdc74164a2dd1cc9354da83ea1ce66d9
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-05-19 21:44:51 -0300

    feat(sort-products): Insert functionality for filter ascending Order and descending order

--- src/js/app.js
+++ src/js/app.js
@@ -2,32 +2,43 @@ import { products } from "./products.js";
 
 const divProducts = document.getElementById('productContainerDisplay');
 const categorySelect = document.getElementById('category');
+const ascendingOrderPrice = document.getElementById('ascendingOrder');
+const descendingOrderPrice = document.getElementById('descendingOrder');
 const buttonFilter = document.getElementById('buttonFilter');
 const allProducts = document.getElementById('listAllProducts');
 
-// Exibe todos os produtos inicialmente
 displayProducts();
 
 allProducts.addEventListener('click', () => displayProducts());
 
-buttonFilter.addEventListener('click', function () {
+buttonFilter.addEventListener('click', () => {
     const selectedCategory = categorySelect.value;
     const selectedAvailability = document.querySelector('input[name="availableFilter"]:checked').value;
-
     displayProducts(selectedCategory, selectedAvailability);
+});
+
+ascendingOrderPrice.addEventListener('click', (event) => {
+    event.preventDefault();
+    products.sort((firstElementToCompare, secondElementToCompare) => 
+        firstElementToCompare.preco - secondElementToCompare.preco);
+    buttonFilter.click();
+});
+
+descendingOrderPrice.addEventListener('click', (event) => {
+    event.preventDefault();
+    products.sort((firstElementToCompare, secondElementToCompare) => 
+        secondElementToCompare.preco - firstElementToCompare.preco);
+    buttonFilter.click();
 });
 
 function displayProducts(category = "", availability = "all") {
     let html = '';
-
     products.forEach(product => {
         const matchesCategory = !category || product.categoria === category;
-
         const matchesAvailability =
             availability === "all" ||
             (availability === "available" && product.disponibilidade) ||
             (availability === "unavailable" && !product.disponibilidade);
-
         if (matchesCategory && matchesAvailability) {
             html += `
             <div class="product">
@@ -39,6 +50,5 @@ function displayProducts(category = "", availability = "all") {
             </div>`;
         }
     });
-
     divProducts.innerHTML = html || "<p>Nenhum produto encontrado com esses filtros.</p>";
 }
